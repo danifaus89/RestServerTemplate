e9ac40250be2ab9ba28144dcc2871d4b932ef370
danifaus89 <danifaus89@hotmail.com>
2023-02-08 12:18:32 +0100

    port update

--- models/server.js
+++ models/server.js
@@ -4,34 +4,34 @@ const cors = require("cors");
 const port = 8080;
 
 class Server {
-	constructor() {
-		this.app = express();
-		this.port = process.env.PORT;
-		this.userPath = "/api/users";
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.userPath = "/api/users";
 
-		//MIDDLEWARES
-		this.middlewares();
-		this.app.use(express.json()); //lectura y parseo del body en json
+        //MIDDLEWARES
+        this.middlewares();
+        this.app.use(express.json()); //lectura y parseo del body en json
 
-		//ROUTES
-		this.routes();
-	}
+        //ROUTES
+        this.routes();
+    }
 
-	middlewares() {
-		this.app.use(cors());
-		//directorio publico
-		this.app.use(express.static("public"));
-	}
+    middlewares() {
+        this.app.use(cors());
+        //directorio publico
+        this.app.use(express.static("public"));
+    }
 
-	routes() {
-		this.app.use(this.userPath, require("../routes/userRoutes"));
-	}
+    routes() {
+        this.app.use(this.userPath, require("../routes/userRoutes"));
+    }
 
-	listen() {
-		this.app.listen(this.port, () => {
-			console.log("Server running on port: ", this.port);
-		});
-	}
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log("Server running on port: ", this.port);
+        });
+    }
 }
 
 module.exports = Server;
